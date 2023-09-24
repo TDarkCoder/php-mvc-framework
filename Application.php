0987d1ac636a0c49205c2fffd2f0ffa3ee563c58
Azizbek Khalilov <azizbekkhalilov151@gmail.com>
2023-09-24 21:08:54 +0500

    Implement for validation rules for Request Class

--- Application.php
+++ Application.php
@@ -7,8 +7,10 @@ use Exception;
 class Application
 {
     public static self $app;
+    public readonly Database $database;
     public readonly Request $request;
     public readonly Router $router;
+    public readonly Session $session;
     public readonly View $view;
 
     public function __construct(public readonly string $rootPath, public readonly array $config)
@@ -20,7 +22,9 @@ class Application
 
     private function initializeComponents(): void
     {
+        $this->database = new Database();
         $this->request = new Request();
+        $this->session = new Session();
         $this->router = new Router($this->request);
         $this->view = new View();
     }
--- Request.php
+++ Request.php
@@ -7,6 +7,7 @@ use TDarkCoder\Framework\Enums\Rules;
 class Request
 {
     protected array $data = [];
+    protected array $errors = [];
 
     public function __construct()
     {
@@ -52,6 +53,75 @@ class Request
     public function method(): string
     {
         return strtolower($this->data['_method'] ?? $_SERVER['REQUEST_METHOD'] ?? '');
+    }
+
+    public function validate(array $data): bool
+    {
+        foreach ($data as $attribute => $rules) {
+            $value = $this->data[$attribute] ?? '';
+
+            $rules = explode('|', $rules);
+
+            foreach ($rules as $rule) {
+                $newRules = explode(':', $rule);
+
+                if (count($newRules) > 1) {
+                    [$rule, $action] = $newRules;
+
+                    if ($rule === Rules::Min->value && strlen($value) < $action) {
+                        $this->addError($attribute, Rules::Min, $action);
+                    }
+
+                    if ($rule === Rules::Max->value && strlen($value) > $action) {
+                        $this->addError($attribute, Rules::Min, $action);
+                    }
+
+                    if ($rule === Rules::LessOrEqual->value && $value > $action) {
+                        $this->addError($attribute, Rules::LessOrEqual, $action);
+                    }
+
+                    if ($rule === Rules::GreaterOrEqual->value && $value < $action) {
+                        $this->addError($attribute, Rules::GreaterOrEqual, $action);
+                    }
+
+                    if ($rule === Rules::Match->value && $value !== $this->{$action}) {
+                        $this->addError($attribute, Rules::Match, $action);
+                    }
+
+                    if ($rule === Rules::Unique->value) {
+                        $object = new $action();
+
+                        if ($object instanceof Model && $object->findOne([$attribute => $value])) {
+                            $this->addError($attribute, Rules::Unique, $attribute);
+                        }
+                    }
+                } else {
+                    if ($rule === Rules::Email->value && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                        $this->addError($attribute, Rules::Email);
+                    }
+
+                    if ($rule === Rules::Required->value && !$value) {
+                        $this->addError($attribute, Rules::Required);
+                    }
+
+                    if ($rule === Rules::Number->value && !is_numeric($value)) {
+                        $this->addError($attribute, Rules::Number);
+                    }
+                }
+            }
+        }
+
+        return empty($this->errors);
+    }
+
+    public function getError(string $attribute): string
+    {
+        return $this->errors[$attribute][0] ?? false;
+    }
+
+    private function addError(string $attribute, Rules $rule, string $action = ''): void
+    {
+        $this->errors[$attribute][] = str_replace("{{$rule->value}}", $action, $rule->message()) ?? 'Unknown error';
     }
 
     public function path(): string
